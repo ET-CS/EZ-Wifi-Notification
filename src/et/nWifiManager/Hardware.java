21b3b35ef89dd196cb4ad2e44c6dde2c5229a61b
ET-CS <et.programming@gmail.com>
2014-07-20 03:50:07 +0300

    change to api 19 and warning fix in airplane_mode

--- src/et/nWifiManager/Hardware.java
+++ src/et/nWifiManager/Hardware.java
@@ -1,9 +1,11 @@
 package et.nWifiManager;
 
 import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.provider.Settings;
 
 public class Hardware {
@@ -14,9 +16,16 @@ public class Hardware {
 	 * @param context
 	 * @return true if enabled.
 	 */
+	@SuppressWarnings("deprecation")
+	@TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
 	public static boolean isAirplaneModeOn(Context context) {
-		return Settings.System.getInt(context.getContentResolver(),
-			Settings.System.AIRPLANE_MODE_ON, 0) != 0;
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+			return Settings.System.getInt(context.getContentResolver(),
+					Settings.Global.AIRPLANE_MODE_ON, 0) != 0;
+		} else {
+			return Settings.System.getInt(context.getContentResolver(),
+					Settings.System.AIRPLANE_MODE_ON, 0) != 0;
+		}
 	}
 
 	public static String getSSID(Context context) {
